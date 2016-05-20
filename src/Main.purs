08605ee2244e5822e81f437e5c8491923ee44bcb
Rogan Murley <rogan@potatolondon.com>
2016-05-20 02:42:25 +0100

    Added maps

--- src/Main.purs
+++ src/Main.purs
@@ -23,16 +23,64 @@ instance showMap :: Show Map where
 
 allMaps :: List Map
 allMaps =
-    Cons (Map "Badlands")   $
-    Cons (Map "Granary")    $
-    Cons (Map "Gravel Pit") $
-    Cons (Map "Gullywash")  $
-    Cons (Map "Metalworks") $
-    Cons (Map "Process")    $
-    Cons (Map "Product")    $
-    Cons (Map "Reckoner")   $
-    Cons (Map "Snakewater") $
-    Cons (Map "Sunshine")   $
+    Cons (Map "2Fort")        $
+    Cons (Map "5Gorge")       $
+    Cons (Map "Asteroid")       $
+    Cons (Map "Badlands")     $
+    Cons (Map "Badwater")     $
+    Cons (Map "Barnblitz")    $
+    Cons (Map "Borneo")       $
+    Cons (Map "Cactus Canyon")$
+    Cons (Map "Coldfront")    $
+    Cons (Map "Degroot Keep") $
+    Cons (Map "Doomsday")     $
+    Cons (Map "Double Cross") $
+    Cons (Map "Dustbowl")     $
+    Cons (Map "Egypt")        $
+    Cons (Map "Fastlane")     $
+    Cons (Map "Foundry")      $
+    Cons (Map "Freight")      $
+    Cons (Map "Frontier")     $
+    Cons (Map "Gold Rush")    $
+    Cons (Map "Gorge")        $
+    Cons (Map "Granary")      $
+    Cons (Map "Gravel Pit")   $
+    Cons (Map "Gullywash")    $
+    Cons (Map "Harvest")      $
+    Cons (Map "Highpass")     $
+    Cons (Map "Hightower")    $
+    Cons (Map "Hoodoo")       $
+    Cons (Map "Hydro")        $
+    Cons (Map "Junction")     $
+    Cons (Map "Kong King")    $
+    Cons (Map "Landfall")     $
+    Cons (Map "Lakeside")     $
+    Cons (Map "Mountain Lab") $
+    Cons (Map "Metalworks")   $
+    Cons (Map "Nightfall")    $
+    Cons (Map "Nucleus")      $
+    Cons (Map "Pipeline")     $
+    Cons (Map "Powerhouse")   $
+    Cons (Map "Probed")       $
+    Cons (Map "Process")      $
+    Cons (Map "Product")      $
+    Cons (Map "Reckoner")     $
+    Cons (Map "Sawmill")      $
+    Cons (Map "Snakewater")   $
+    Cons (Map "Snowplow")     $
+    Cons (Map "Snowycoast")   $
+    Cons (Map "Standin")      $
+    Cons (Map "Steel")        $
+    Cons (Map "Suijin")       $
+    Cons (Map "Sunshine")     $
+    Cons (Map "Thunder Mountain") $
+    Cons (Map "Turbine")      $
+    Cons (Map "Upward")       $
+    Cons (Map "Vanguard")     $
+    Cons (Map "Viaduct")      $
+    Cons (Map "Watergate")    $
+    Cons (Map "Well")         $
+    Cons (Map "Yukon")        $
     Nil
 
 
@@ -43,7 +91,13 @@ stratLefty :: Strat
 stratLefty = Strat "Curse of the Lefty" "Use left-handed viewmodels."
 
 stratPub :: Strat
-stratPub = Strat "Normal Pub" "Whole team can only play Sniper or Spy."
+stratPub = Strat "Valve Server" "Whole team can only play Sniper or Spy."
 
 stratTrueFriends :: Strat
 stratTrueFriends = Strat "True Friends" "If your medic dies, killbind."
+
+stratTrain :: Strat
+stratTrain = Strat "Train" "Unbind strafe keys."
+
+stratDegroot :: Strat
+stratDegroot = Strat "Straight Outta Degroot" "Medieval mode loadouts only."
