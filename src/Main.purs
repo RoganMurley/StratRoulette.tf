32239a2e4ad09b379599f986090e2d4bf24589ea
Rogan Murley <rogan@potatolondon.com>
2016-05-20 13:21:44 +0100

    Added show and fromJSON typeclasses to maps and strats

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,13 @@
 module Main where
 
-import Network.HTTP.Affjax
 import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Prelude (($), (++), bind, Unit)
+import Data.JSON ((.:), fail, class FromJSON, JValue(JObject))
+import Network.HTTP.Affjax
+import Prelude (($), (<$>), (<*>), (++), bind, class Show, Unit)
 
 
 main :: Eff (ajax :: AJAX, console :: CONSOLE, err :: EXCEPTION) Unit
@@ -26,8 +27,24 @@ type MM    = Prim.Boolean
 -- Maps.
 data Map = Map Name Sixes HL MM
 
--- instance showMap :: Show Map where
-  -- show (Map name _ _ _) = name
+instance showMap :: Show Map where
+    show (Map name _ _ _) = name
+
+instance mapFromJSON :: FromJSON Map where
+    parseJSON (JObject o) =
+        Map <$> (o .: "name")
+            <*> (o .: "available.sixes")
+            <*> (o .: "available.hl")
+            <*> (o .: "available.mm")
+    parseJSON _           = fail "Map parse failed."
+
 
 -- Strats.
 data Strat = Strat Name Desc
+
+instance showStrat :: Show Strat where
+    show (Strat name desc) = name ++ ": " ++ desc
+
+instance stratFromJSON :: FromJSON Strat where
+    parseJSON (JObject o) = Strat <$> (o .: "name") <*> (o .: "desc")
+    parseJSON _           = fail "Strat parse failed."
