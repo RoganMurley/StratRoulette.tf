8ae89c69d65f20746c9823c4c3db832ad8addd9f
Rogan Murley <rogan@potatolondon.com>
2016-05-20 22:10:51 +0100

    Added some css and stuff

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,6 @@
 module Main where
 
+import Flare
 import Control.Bind ((>=>))
 import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
@@ -7,23 +8,28 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (randomInt, RANDOM)
+import DOM (DOM)
 import Data.Array (index)
 import Data.Either.Unsafe (fromRight)
 import Data.Foreign (F)
 import Data.Foreign.Class (class IsForeign, readJSON, readProp)
 import Data.Foreign.Index (prop)
-import Network.HTTP.Affjax
-import Prelude (($), (<>), (#), bind, pure, class Show, show, Unit)
+import Network.HTTP.Affjax (AJAX, get)
+import Prelude (($), (<>), (#), (+), bind, pure, class Show, show, Unit)
+import Signal.Channel (CHANNEL)
 
-main :: Eff (ajax :: AJAX, console :: CONSOLE, err :: EXCEPTION, random :: RANDOM) Unit
-main = launchAff do
-        res <- get "./api/strats"
-        n <- liftEff $ randomInt 0 6
-        liftEff $ log $ show $ index (stratsFromRes res.response) n
-    where
-        stratsFromRes res = fromRight (readJSON res :: F (Array Strat))
+-- main :: Eff (ajax :: AJAX, channel :: CHANNEL, console :: CONSOLE, dom :: DOM, err :: EXCEPTION, random :: RANDOM) Unit
+-- main = launchAff do
+--         res <- get "./api/strats"
+--         n <- liftEff $ randomInt 0 6
+--         liftEff $ log $ show $ index (stratsFromRes res.response) n
+--         liftEff $ runFlareShow "controls" "output" counter
+--     where
+--         stratsFromRes res = fromRight (readJSON res :: F (Array Strat))
 
+main = runFlareShow "controls" "output" counter
 
+counter = foldp (+) 0 (button "Increment" 0 1)
 
 -- Maps.
 data Map =
