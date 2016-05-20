4a19b622f5a87b8aae91f1c0f56a637e4bc758f9
Rogan Murley <rogan@potatolondon.com>
2016-05-20 02:25:27 +0100

    Basic framework

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,47 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.List (List(Cons, Nil))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-    log "Hello sailor!"
-    log "lol"
+    log "Hello"
 
 
-foo :: Int
-foo = 1
+-- Type synonyms.
+type Name = String
+type Desc = String
+
+
+-- Maps.
+data Map = Map Name
+
+instance showMap :: Show Map where
+  show (Map name) = name
+
+allMaps :: List Map
+allMaps =
+    Cons (Map "Badlands")   $
+    Cons (Map "Granary")    $
+    Cons (Map "Gravel Pit") $
+    Cons (Map "Gullywash")  $
+    Cons (Map "Metalworks") $
+    Cons (Map "Process")    $
+    Cons (Map "Product")    $
+    Cons (Map "Reckoner")   $
+    Cons (Map "Snakewater") $
+    Cons (Map "Sunshine")   $
+    Nil
+
+
+-- Starts.
+data Strat = Strat Name Desc
+
+stratLefty :: Strat
+stratLefty = Strat "Curse of the Lefty" "Use left-handed viewmodels."
+
+stratPub :: Strat
+stratPub = Strat "Normal Pub" "Whole team can only play Sniper or Spy."
+
+stratTrueFriends :: Strat
+stratTrueFriends = Strat "True Friends" "If your medic dies, killbind."
