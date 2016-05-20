e09062407b4c0ac0b4de59c45c39fe16ba210b36
Rogan Murley <rogan@potatolondon.com>
2016-05-20 03:02:51 +0100

    Added tags for how maps are played

--- src/Main.purs
+++ src/Main.purs
@@ -11,76 +11,81 @@ main = do
 
 
 -- Type synonyms.
-type Name = String
-type Desc = String
+type Name  = String
+type Desc  = String
+type Sixes = Prim.Boolean
+type HL    = Prim.Boolean
+type MM    = Prim.Boolean
 
 
 -- Maps.
-data Map = Map Name
+data Map = Map Name Sixes HL MM
 
 instance showMap :: Show Map where
-  show (Map name) = name
+  show (Map name _ _ _) = name
 
 allMaps :: List Map
 allMaps =
-    Cons (Map "2Fort")        $
-    Cons (Map "5Gorge")       $
-    Cons (Map "Asteroid")       $
-    Cons (Map "Badlands")     $
-    Cons (Map "Badwater")     $
-    Cons (Map "Barnblitz")    $
-    Cons (Map "Borneo")       $
-    Cons (Map "Cactus Canyon")$
-    Cons (Map "Coldfront")    $
-    Cons (Map "Degroot Keep") $
-    Cons (Map "Doomsday")     $
-    Cons (Map "Double Cross") $
-    Cons (Map "Dustbowl")     $
-    Cons (Map "Egypt")        $
-    Cons (Map "Fastlane")     $
-    Cons (Map "Foundry")      $
-    Cons (Map "Freight")      $
-    Cons (Map "Frontier")     $
-    Cons (Map "Gold Rush")    $
-    Cons (Map "Gorge")        $
-    Cons (Map "Granary")      $
-    Cons (Map "Gravel Pit")   $
-    Cons (Map "Gullywash")    $
-    Cons (Map "Harvest")      $
-    Cons (Map "Highpass")     $
-    Cons (Map "Hightower")    $
-    Cons (Map "Hoodoo")       $
-    Cons (Map "Hydro")        $
-    Cons (Map "Junction")     $
-    Cons (Map "Kong King")    $
-    Cons (Map "Landfall")     $
-    Cons (Map "Lakeside")     $
-    Cons (Map "Mountain Lab") $
-    Cons (Map "Metalworks")   $
-    Cons (Map "Nightfall")    $
-    Cons (Map "Nucleus")      $
-    Cons (Map "Pipeline")     $
-    Cons (Map "Powerhouse")   $
-    Cons (Map "Probed")       $
-    Cons (Map "Process")      $
-    Cons (Map "Product")      $
-    Cons (Map "Reckoner")     $
-    Cons (Map "Sawmill")      $
-    Cons (Map "Snakewater")   $
-    Cons (Map "Snowplow")     $
-    Cons (Map "Snowycoast")   $
-    Cons (Map "Standin")      $
-    Cons (Map "Steel")        $
-    Cons (Map "Suijin")       $
-    Cons (Map "Sunshine")     $
-    Cons (Map "Thunder Mountain") $
-    Cons (Map "Turbine")      $
-    Cons (Map "Upward")       $
-    Cons (Map "Vanguard")     $
-    Cons (Map "Viaduct")      $
-    Cons (Map "Watergate")    $
-    Cons (Map "Well")         $
-    Cons (Map "Yukon")        $
+    Cons (Map "2Fort"             false false false) $
+    Cons (Map "5Gorge"            false false false) $
+    Cons (Map "Ashville"          false true  false) $
+    Cons (Map "Asteroid"          false false false) $
+    Cons (Map "Badlands"          true  false true ) $
+    Cons (Map "Badwater"          false true  false) $
+    Cons (Map "Barnblitz"         false true  false) $
+    Cons (Map "Borneo"            false true  false) $
+    Cons (Map "Cactus Canyon"     false false false) $
+    Cons (Map "Coldfront"         false false false) $
+    Cons (Map "Degroot Keep"      false false false) $
+    Cons (Map "Doomsday"          false false false) $
+    Cons (Map "Double Cross"      false false false) $
+    Cons (Map "Dustbowl"          false false false) $
+    Cons (Map "Egypt"             false false false) $
+    Cons (Map "Fastlane"          false false false) $
+    Cons (Map "Foundry"           false false true ) $
+    Cons (Map "Freight"           false false false) $
+    Cons (Map "Frontier"          false false false) $
+    Cons (Map "Gold Rush"         false false false) $
+    Cons (Map "Gorge"             false false true ) $
+    Cons (Map "Granary"           true  false true ) $
+    Cons (Map "Gravel Pit"        false false false) $
+    Cons (Map "Gullywash"         true  true  true ) $
+    Cons (Map "Harvest"           false false false) $
+    Cons (Map "Highpass"          false false false) $
+    Cons (Map "Hightower"         false false false) $
+    Cons (Map "Hoodoo"            false false false) $
+    Cons (Map "Hydro"             false false false) $
+    Cons (Map "Junction"          false false false) $
+    Cons (Map "Kong King"         false false false) $
+    Cons (Map "Landfall"          false false false) $
+    Cons (Map "Lakeside"          false true true  ) $
+    Cons (Map "Mountain Lab"      false false false) $
+    Cons (Map "Metalworks"        true  false false) $
+    Cons (Map "Nightfall"         false false false) $
+    Cons (Map "Nucleus"           false false false) $
+    Cons (Map "Pipeline"          false false false) $
+    Cons (Map "Powerhouse"        false false false) $
+    Cons (Map "Probed"            false false false) $
+    Cons (Map "Process"           true  false true ) $
+    Cons (Map "Ramjam"            false true  false) $
+    Cons (Map "Reckoner"          true  false false) $
+    Cons (Map "Sawmill"           false false false) $
+    Cons (Map "Snakewater"        true  false true ) $
+    Cons (Map "Snowplow"          false false false) $
+    Cons (Map "Snowycoast"        false false false) $
+    Cons (Map "Standin"           false false false) $
+    Cons (Map "Steel"             false true  false) $
+    Cons (Map "Suijin"            false false false) $
+    Cons (Map "Sunshine"          true  false false) $
+    Cons (Map "Swiftwater"        false true  false) $
+    Cons (Map "Thunder Mountain"  false false false) $
+    Cons (Map "Turbine"           false false false) $
+    Cons (Map "Upward"            false true  false) $
+    Cons (Map "Vanguard"          false true  true ) $
+    Cons (Map "Viaduct"           true  true  true ) $
+    Cons (Map "Watergate"         false false false) $
+    Cons (Map "Well"              false false false) $
+    Cons (Map "Yukon"             false false false) $
     Nil
 
 
@@ -93,8 +98,8 @@ stratLefty = Strat "Curse of the Lefty" "Use left-handed viewmodels."
 stratPub :: Strat
 stratPub = Strat "Valve Server" "Whole team can only play Sniper or Spy."
 
-stratTrueFriends :: Strat
-stratTrueFriends = Strat "True Friends" "If your medic dies, killbind."
+strattrueFriends :: Strat
+strattrueFriends = Strat "true Friends" "If your medic dies, killbind."
 
 stratTrain :: Strat
 stratTrain = Strat "Train" "Unbind strafe keys."
