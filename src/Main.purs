368af6857d256a604749e03193495d5882a35593
Rogan Murley <rogan@potatolondon.com>
2016-05-21 14:08:55 +0100

    Very basic rolling

--- src/Main.purs
+++ src/Main.purs
@@ -2,19 +2,22 @@ module Main where
 
 
 import Flare
+-- import Flare.Smolder (runFlareHTML)
+-- import Text.Smolder.Markup as H
+
 import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (randomInt, RANDOM)
 import DOM (DOM)
-import Data.Array (index)
+import Data.Array (index, length)
 import Data.Either.Unsafe (fromRight)
 import Data.Foreign (F)
 import Data.Foreign.Class (readJSON)
 import Network.HTTP.Affjax (AJAX, get)
-import Prelude (($), bind, pure, show, Unit)
+import Prelude (($), (-), bind, pure, show, Unit)
 import Signal.Channel (CHANNEL)
 import Strat (Strat)
 
@@ -22,8 +25,6 @@ import Strat (Strat)
 main :: Eff (ajax :: AJAX, channel :: CHANNEL, console :: CONSOLE, dom :: DOM, err :: EXCEPTION, random :: RANDOM) Unit
 main = launchAff do
         res <- get "./api/strats"
-        -- n <- liftEff $ randomInt 0 6
-        -- liftEff $ log $ show $ index (stratsFromRes res.response) n
         strats <- pure $ stratsFromRes res.response
         liftEff $ interface strats
     where
@@ -31,19 +32,20 @@ main = launchAff do
 
 
 interface :: forall e. Array Strat -> Eff (channel :: CHANNEL, dom :: DOM, random :: RANDOM | e) Unit
-interface strats = runFlareWith "controls_a" handler (intRange "numPoints" 1 100 10)
+interface strats = runFlareWith
+    "controls"
+    (handler strats)
+    (button "Roll" true false)
 
 
-handler :: forall e. Int -> Eff (channel :: CHANNEL, dom :: DOM, random :: RANDOM | e) Unit
-handler n = do
-    n <- randomInt 0 6
-    runFlareShow "controls_b" "output" (counter n)
+handler :: forall e. Array Strat -> Boolean -> Eff (dom :: DOM, channel :: CHANNEL, random :: RANDOM | e) Unit
+handler strats b = do
+    n <- randomInt 0 ((length strats) - 1)
+    runFlareShow "controls_b" "output" (pure $ roll strats n b)
 
 
-counter :: forall e. Int -> UI e String
-counter n = foldp (getValue) s (button "Roll" "" s)
-    where s = show n :: String
-
-
-getValue :: String -> String -> String
-getValue new _ = new
+roll :: Array Strat -> Int -> Boolean -> String
+roll _      _ false  = ""
+roll strats i true = show strat
+    where
+        strat = index strats i
